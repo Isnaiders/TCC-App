527e7c9b2f0b72c24f1264ef6685db2f19bdc904
Isnaiders <henriquermt@gmail.com>
2023-03-13 19:48:14 -0300

    Iniciação do Projeto

---
+++ TCC App/TCC App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TCC_App.ViewModels;
+using Xamarin.Forms;
+
+namespace TCC_App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
