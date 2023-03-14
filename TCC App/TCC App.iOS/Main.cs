072a18f64f4384544461d9df00301089561a67d6
Isnaiders <henriquermt@gmail.com>
2023-03-13 23:16:23 -0300

    Implementação do Google Maps - parte 2

--- TCC App/TCC App.iOS/Main.cs
+++ TCC App/TCC App.iOS/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Google.Maps;
 using UIKit;
 
 namespace TCC_App.iOS
@@ -12,8 +13,10 @@ namespace TCC_App.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            string apiKey = "AIzaSyAd2xyxECeldukvPhSMZWquHGzk9ZNQL58";
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
+            Xamarin.FormsGoogleMaps.Init(apiKey);
             UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
