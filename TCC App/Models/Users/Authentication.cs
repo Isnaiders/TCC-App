3e2f8b212367bd662dfca84ecd2982feecff9893
VitorHSO <vhugo.soc@gmail.com>
2023-04-14 00:01:18 -0300

    Criação dos modelos

---
+++ TCC App/Models/Users/Authentication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC_App.Models.Users
+{
+    public class Authentication
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+}
