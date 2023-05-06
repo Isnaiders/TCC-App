01503b49dd2aca1d30ca815e7544fad49db6d4d4
VitorHSO <vhugo.soc@gmail.com>
2023-05-06 16:41:51 -0300

    Criação das classes básicas

---
+++ TCC Web/Models/Parking/Parking.cs
@@ -0,0 +1,12 @@
+﻿using TCC_Web.Models.Base;
+
+namespace TCC_Web.Models.Parking
+{
+    public class Parking : BaseModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
---
+++ TCC Web/Models/User/Provider.cs
@@ -0,0 +1,6 @@
+﻿namespace TCC_Web.Models.User
+{
+    public class Provider : User
+    {
+    }
+}
