d5888b18ff70a8fca7c01a0d4e95cb4e7a7dfcd2
Isnaiders <henriquermt@gmail.com>
2023-05-11 22:00:48 -0300

    Configuração inicial do banco de dados

--- TCC API/Program.cs
+++ TCC API/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TCC_API.Models.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TC_EstaVagaContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
