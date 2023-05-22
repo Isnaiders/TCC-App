7a41e1a96a23b391b73e18871fdd5e4c814e1b4a
felipe020502 <lipeguilherme06@gmail.com>
2023-05-22 20:15:18 -0300

    Validações e redirecionamento para a Index da Home

--- TCC Web/Controllers/LoginController.cs
+++ TCC Web/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TCC_Web.Models;
 
 namespace TCC_Web.Controllers
 {
@@ -7,6 +8,25 @@ namespace TCC_Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return View("Index");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = $"Ops, algo deu errado! Por favor, tente novamente mais tarde: {ex.Message}";
+                throw;
+            }
         }
     }
 }
---
+++ TCC Web/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCC_Web.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Digite o Usuario")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Digite a Senha")]
+        public string Password { get; set; }
+    }
+}
