7966b65dc0e594e7e1bd30e21d634c3f170c217d
felipe020502 <lipeguilherme06@gmail.com>
2023-05-22 21:17:31 -0300

    Propriedade voltou a se chamar UserName

--- TCC Web/Models/DTOs/Login/LoginDTO.cs
+++ TCC Web/Models/DTOs/Login/LoginDTO.cs
@@ -4,8 +4,8 @@ namespace TCC_Web.Models.DTOs.Login
 {
     public class LoginDTO
     {
-        [Required(ErrorMessage = "Digite o Email")]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Digite o Usuario")]
+        public string UserName { get; set; }
 
         [Required(ErrorMessage = "Digite a Senha")]
         public string Password { get; set; }
